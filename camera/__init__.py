b5a900373ddf0bbba3b0f3f9205d60d32eafc880
Boyan Zarev <zarev.bojan2@gmail.com>
2025-06-10 23:28:18 +0300

    add moving camera left and right

--- camera/__init__.py
+++ camera/__init__.py
@@ -10,7 +10,7 @@ class Camera:
 
         self.possition = Point(self.x, self.y, self.z, BLACK)
 
-        self.speed = 1
+        self.speed = 3
 
     def move(self, key_pressed, bodies):
         if key_pressed[pg.K_w]:
@@ -28,6 +28,22 @@ class Camera:
             for b in bodies:
                 for p in b.points:
                     p.update_cords((p.x, p.y, p.z+self.speed))
+
+        if key_pressed[pg.K_a]:
+            self.x += self.speed
+            self.update_possition()
+
+            for b in bodies:
+                for p in b.points:
+                    p.update_cords((p.x+self.speed, p.y, p.z))
+
+        if key_pressed[pg.K_d]:
+            self.x -= self.speed
+            self.update_possition()
+
+            for b in bodies:
+                for p in b.points:
+                    p.update_cords((p.x-self.speed, p.y, p.z))
 
     def update_possition(self):
         self.possition = Point(self.x, self.y, self.z, BLACK)
